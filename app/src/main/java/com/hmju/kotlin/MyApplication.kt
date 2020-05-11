7997378520be687a35df4f56a4f96bdd7e14c7e6
Sieun Ju <j.sieun73@gmail.com>
2020-05-11 21:10:47 +0900

    2020.05.11 주홍민 - Release_1.0.4 버전 추가.

---
+++ app/src/main/java/com/hmju/kotlin/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.hmju.kotlin
+
+import android.app.Application
+
+/**
+ * Description: MyApplication Class
+ *
+ * Created by juhongmin on 2020/05/11
+ */
+class MyApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
